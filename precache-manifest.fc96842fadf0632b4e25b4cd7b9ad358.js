cf1962c1f0eb326b593d8a8fd87e87176c3c721f
unknown <sank.shetye@gmail.com>
2020-02-08 19:06:09 +0530

    Updates

---
+++ precache-manifest.fc96842fadf0632b4e25b4cd7b9ad358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53083163492f676d5c764bc166e6d72d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e2ab0050de692ac6eb67",
+    "url": "/monsters-rolodex/static/css/main.9fd8c256.chunk.css"
+  },
+  {
+    "revision": "004ed19f24e36f1c89ea",
+    "url": "/monsters-rolodex/static/js/2.09b00f4e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.09b00f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2ab0050de692ac6eb67",
+    "url": "/monsters-rolodex/static/js/main.583eaf8f.chunk.js"
+  },
+  {
+    "revision": "f675242c94ba7ef58f71",
+    "url": "/monsters-rolodex/static/js/runtime-main.2a8166e1.js"
+  }
+]);
